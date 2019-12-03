18716ea52bbd9988c92b5a7f1989d1117ecf7717
amgalan-sp <darzhaev@gmail.com>
2019-12-03 22:38:13 +0900

    Fixed naming for variables func_hh, func_sj to table_data

--- salary.py
+++ salary.py
@@ -6,18 +6,18 @@ from salary_sj import get_salary_sj
 def main():
     top_languages_list=('JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'Go', 'Shell', 'Objective-C',
                         'Scala', 'Swift', 'TypeScript')
-    func_hh = [list(get_salary_hh(programming_language)) for programming_language in top_languages_list]
-    func_sj = [list(get_salary_sj(programming_language)) for programming_language in top_languages_list]
+    table_data_hh = [list(get_salary_hh(programming_language)) for programming_language in top_languages_list]
+    table_data_sj = [list(get_salary_sj(programming_language)) for programming_language in top_languages_list]
     title_hh = 'HH Moscow'
     title_sj = 'SJ Moscow'
     print(get_table(title_hh, func_hh))
     print(get_table(title_sj, func_sj))
 
 
-def get_table(title, func):
+def get_table(title, table_data):
     indicators_list = ['Язык программирования', 'Средняя зарплата', 'Найдено вакансий', 'Обработано вакансий']
-    func.insert(0, indicators_list)
-    table = AsciiTable(func, title)
+    table_data.insert(0, indicators_list)
+    table = AsciiTable(table_data, title)
     return table.table
 
 if __name__ == '__main__':
